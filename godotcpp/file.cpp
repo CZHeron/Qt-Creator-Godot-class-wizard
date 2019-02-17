90df303995cb45f187c02a9e8c03531acde32732
CZHeron <czheron@gmail.com>
2019-02-17 22:05:16 +0100

    Added an option for override functions

--- godotcpp/file.cpp
+++ godotcpp/file.cpp
@@ -1,24 +1,71 @@
 %{Cpp:LicenseTemplate}\
 #include "%{HdrFileName}"
 %{JS: Cpp.openNamespaces('%{Class}')}
+@if "%{_enter_tree}" == "true"
+void %{CN}::_enter_tree()
+{
+
+}
+
+@endif
+@if "%{_init}" == "true"
 void %{CN}::_init()
 {
 
 }
+
+@endif
+@if "%{_ready}" == "true"
 void %{CN}::_ready()
 {
 
 }
 
+@endif
+@if "%{_input}" == "true"
+void %{CN}::_input(Ref<InputEvent> event)
+{
+
+}
+
+@endif
+@if "%{_unhandled_input}" == "true"
+void %{CN}::_unhandled_input(Ref<InputEvent> event)
+{
+
+}
+
+@endif
+@if "%{_unhandled_key_input}" == "true"
+void %{CN}::_unhandled_key_input(Ref<InputEventKey> event)
+{
+
+}
+
+@endif
+@if "%{_process}" == "true"
 void %{CN}::_process(float delta)
 {
 
 }
 
+@endif
+@if "%{_physics_process}" == "true"
+void %{CN}::_physics_process(float delta)
+{
+
+}
+
+@endif
 void %{CN}::_register_methods()
 {
     register_method(const_cast<char *>("_init"), &%{CN}::_init);
     register_method(const_cast<char *>("_ready"), &%{CN}::_ready);
     register_method(const_cast<char *>("_process"), &%{CN}::_process);
+}
+
+%{CN}::~%{CN}()
+{
+
 }
 %{JS: Cpp.closeNamespaces('%{Class}')}\
--- godotcpp/file.h
+++ godotcpp/file.h
@@ -4,6 +4,12 @@
 
 #include <core/Godot.hpp>
 #include <gen/%{Base}.hpp>
+@if "%{_input}" == "true" || "%{_unhandled_input}" == "true"
+#include <gen/InputEvent.hpp>
+@endif
+@if "%{_unhandled_key_input}" == "true"
+#include <gen/InputEventKey.hpp>
+@endif
 %{JS: Cpp.openNamespaces('%{Class}')}
 @if "%{UseNamespace}" == "true"
 using namespace godot;
@@ -17,17 +23,44 @@ class %{CN} : public godot::%{Base}
     GODOT_CLASS(%{CN}, godot::%{Base});
 @endif
 public:
-%{CN}(){};
-    //Called when the node is created
+    %{CN}(){}
+@if "%{_enter_tree}" == "true"
+    //Called when the node enters the SceneTree.
+    void _enter_tree();
+@endif
+@if "%{_init}" == "true"
+    //Called when the node is created.
     void _init();
-
-    //Called when the node is ready
+@endif
+@if "%{_ready}" == "true"
+    //Called when the node is ready.
     void _ready();
-
-    //Called during the processing step of the main loop
-    void _process(float delta);
-
+@endif
+@if "%{_input}" == "true"
+    //Called when there is an input event. The input event propagates up through the node tree until a node consumes it.
+    void _input(Ref<InputEvent> event);
+@endif
+@if "%{_unhandled_input}" == "true"
+    //Called when an InputEvent hasn’t been consumed by _input or any GUI.
+    //The input event propagates up through the node tree until a node consumes it.
+    void _unhandled_input(Ref<InputEvent> event);
+@endif
+@if "%{_unhandled_key_input}" == "true"
+    //Called when an InputEventKey hasn’t been consumed by _input or any GUI.
+    //The input event propagates up through the node tree until a node consumes it.
+    void _unhandled_key_input(Ref<InputEventKey> event);
+@endif
+@if "%{_process}" == "true"
+    //Called during the processing step of the main loop.
+void _process(float delta);
+@endif
+@if "%{_physics_process}" == "true"
+    //Called during the physics processing step of the main loop.
+    void _physics_process(float delta);
+@endif
     static void _register_methods();
+
+    ~%{CN}();
 };
 %{JS: Cpp.closeNamespaces('%{Class}')}
 #endif // %{GUARD}\
