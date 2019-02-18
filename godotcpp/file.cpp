bd1a3ce5896f75be0e557d77616171285bafb8d4
CZHeron <czheron@gmail.com>
2019-02-18 04:07:20 +0100

    Fixing _register_methods()

--- godotcpp/file.cpp
+++ godotcpp/file.cpp
@@ -59,9 +59,30 @@ void %{CN}::_physics_process(float delta)
 @endif
 void %{CN}::_register_methods()
 {
+@if "%{_enter_tree}" == "true"
+    register_method(const_cast<char *>("_enter_tree"), &%{CN}::_enter_tree);
+@endif
+@if "%{_init}" == "true"
     register_method(const_cast<char *>("_init"), &%{CN}::_init);
+@endif
+@if "%{_ready}" == "true"
     register_method(const_cast<char *>("_ready"), &%{CN}::_ready);
+@endif
+@if "%{_input}" == "true"
+    register_method(const_cast<char *>("_input"), &%{CN}::_input);
+@endif
+@if "%{_unhandled_input}" == "true"
+    register_method(const_cast<char *>("_unhandled_input"), &%{CN}::_unhandled_input);
+@endif
+@if "%{_unhandled_key_input}" == "true"
+    register_method(const_cast<char *>("_unhandled_key_input"), &%{CN}::_unhandled_key_input);
+@endif
+@if "%{_process}" == "true"
     register_method(const_cast<char *>("_process"), &%{CN}::_process);
+@endif
+@if "%{_physics_process}" == "true"
+    register_method(const_cast<char *>("_physics_process"), &%{CN}::_physics_process);
+@endif
 }
 
 %{CN}::~%{CN}()
